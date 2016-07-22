a8d5d522b5b7887082c921c402a389e8890eac16
nickpace <pacenick@gmail.com>
2016-07-22 10:49:12 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -3,9 +3,30 @@
 
 [![Build Status](https://travis-ci.org/hmrc/microservice-async.svg?branch=master)](https://travis-ci.org/hmrc/microservice-async) [ ![Download](https://api.bintray.com/packages/hmrc/releases/microservice-async/images/download.svg) ](https://bintray.com/hmrc/releases/microservice-async/_latestVersion)
 
-This is a placeholder README.md for a new repository
+Library extends the play-async library to provide extensions for both a mongo cache repository and the filter SessionCookieCryptoFilter is used to support state between service API calls.
+
+
+
+## Installing
+
+Include the following dependency in your SBT build
+
+``` scala
+resolvers += Resolver.bintrayRepo("hmrc", "releases")
+
+libraryDependencies += "uk.gov.hmrc" %% "microservice-async" % "[INSERT-VERSION]"
+```
+
+## Configuration
+
+In `/conf/play.plugins` add the reference to `MicroServiceAsyncPlugin`
+
+```scala
+    5000:uk.gov.hmrc.msasync.plugin.MicroServiceAsyncPlugin
+```
+
+*Note: If 5000 is already in use choose another number*
 
 ### License
 
 This code is open source software licensed under the [Apache 2.0 License]("http://www.apache.org/licenses/LICENSE-2.0.html").
-    
---
+++ project/HmrcBuild.scala
@@ -0,0 +1,51 @@
+import sbt.Keys._
+import sbt._
+
+object HmrcBuild extends Build {
+
+  import uk.gov.hmrc.DefaultBuildSettings._
+  import uk.gov.hmrc.SbtAutoBuildPlugin
+  import uk.gov.hmrc.versioning.SbtGitVersioning
+
+
+  val nameApp = "microservice-async"
+
+  lazy val library = Project(nameApp, file("."))
+    .enablePlugins(play.PlayScala, SbtAutoBuildPlugin, SbtGitVersioning)
+    .settings(scalaSettings: _*)
+    .settings(defaultSettings(): _*)
+    .settings(
+      libraryDependencies ++= AppDependencies(),
+      crossScalaVersions := Seq("2.11.7")
+    )
+    .settings(resolvers += Resolver.bintrayRepo("hmrc", "releases"))
+}
+
+private object AppDependencies {
+
+  import play.PlayImport._
+  import play.core.PlayVersion
+
+  val compile = Seq(
+    "uk.gov.hmrc" %% "play-async" % "0.3.0",
+    "uk.gov.hmrc" %% "crypto" % "3.1.0",
+    "uk.gov.hmrc" %% "play-filters" % "4.6.0",
+    "uk.gov.hmrc" %% "play-config" % "2.0.1",
+    "uk.gov.hmrc" %% "play-json-logger" % "2.1.1",
+    "uk.gov.hmrc" %% "play-reactivemongo" % "4.8.0"
+  )
+
+  val testScope: String = "test"
+
+  val test = Seq(
+    "org.scalatest" %% "scalatest" % "2.2.1" % testScope,
+    "org.pegdown" % "pegdown" % "1.4.2" % testScope,
+    "uk.gov.hmrc" %% "hmrctest" % "1.7.0" % testScope,
+    "uk.gov.hmrc" %% "play-config" % "2.0.1" % testScope,
+    "uk.gov.hmrc" %% "reactivemongo-test" % "1.6.0" % testScope
+  )
+
+  def apply() = compile ++ test
+}
+
+

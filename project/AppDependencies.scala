6c036c60d16b6209e4228ac19a96ba6291ef7a2d
Luke Smith <luke.smith@digital.hmrc.gov.uk>
2018-08-08 14:07:35 +0100

    [LS] Upgraded mongo & other libraries

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
     "uk.gov.hmrc" %% "crypto" % "4.4.0",
     "uk.gov.hmrc" %% "play-filters" % "5.18.0",
     "uk.gov.hmrc" %% "play-config" % "4.3.0",
-    "uk.gov.hmrc" %% "play-reactivemongo" % "5.2.0"
+    "uk.gov.hmrc" %% "play-reactivemongo" % "6.2.0"
   )
 
   trait TestDependencies {
@@ -22,8 +22,8 @@ object AppDependencies {
     def apply() = new TestDependencies {
       override lazy val test = Seq(
         "org.scalatest" %% "scalatest" % "3.0.1" % scope,
-        "uk.gov.hmrc" %% "hmrctest" % "2.3.0" % scope,
-        "uk.gov.hmrc" %% "reactivemongo-test" % "1.6.0" % scope,
+        "uk.gov.hmrc" %% "hmrctest" % "3.0.0" % scope,
+        "uk.gov.hmrc" %% "reactivemongo-test" % "3.1.0" % scope,
         "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
         "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % scope,
         "org.mockito" % "mockito-all" % "1.9.5" % scope
--- src/main/scala/uk/gov/hmrc/msasync/repository/AsyncMongoRepository.scala
+++ src/main/scala/uk/gov/hmrc/msasync/repository/AsyncMongoRepository.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,6 +27,7 @@ import uk.gov.hmrc.play.asyncmvc.model.TaskCache
 import uk.gov.hmrc.time.DateTimeUtils
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{ExecutionContext, Future}
+import reactivemongo.play.json.ImplicitBSONHandlers._
 
 case class TaskCachePersist(id: BSONObjectID, task:TaskCache)
 
